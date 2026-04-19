ea683553b1e410c3ea8e39e4a8f23b7a9e42b282
Clarence Liong <clarenceliong0815@gmail.com>
2026-04-19 14:50:51 +0800

    Delete test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout<<"Hi"<<endl;
-    cout<<"Welcome to this project"<<endl;
-    cout<<"This is the Clarence' repository"<<endl;
-
-    return 0;
-}
